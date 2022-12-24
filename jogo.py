d4060f68f18574dd57fd98fab30595b219e51f1d
Carol42 <caroline.heliose@gmail.com>
2022-12-24 18:20:31 -0300

    adiciona boneco

--- jogo.py
+++ jogo.py
@@ -3,7 +3,7 @@ import random
 
 def sortear_palavra():
 
-    palavraOculta = ""
+    palavra_oculta = ""
 
     with open("palavras.txt") as arquivo:
         itens = arquivo.readlines()
@@ -11,22 +11,36 @@ def sortear_palavra():
     indice = random.randint(0, len(itens) - 1)
     palavra, dica = itens[indice].strip().split(",")
 
-    palavraOculta = "-" * len(palavra)
+    palavra_oculta = "-" * len(palavra)
 
-    return palavra, dica, palavraOculta
+    return palavra, dica, palavra_oculta
 
 def atualiza_palavra():
     global letra
     global palavra
     global letras_acertadas
 
-    palavra_oculta = ""
+    palavra_atualizada = ""
     for letra in palavra:
         if letra in letras_acertadas:
-            palavra_oculta += letra
+            palavra_atualizada += letra
         else:
-            palavra_oculta += "-"
-    return palavra_oculta
+            palavra_atualizada += "-"
+    return palavra_atualizada
+
+def esquarteja(erros):
+    if erros == 1:
+        canvas.delete(perna_dir)
+    elif erros == 2:
+        canvas.delete(perna_esq)
+    elif erros == 3:
+        canvas.delete(braco_dir)
+    elif erros == 4:
+        canvas.delete(braco_esq)
+    elif erros == 5:
+        canvas.delete(tronco)
+    else:
+        canvas.delete(cabeca)
 
 def jogar():
     global letra
@@ -37,11 +51,11 @@ def jogar():
 
     if not letra_entry.get().isalpha() or len(letra_entry.get()) != 1:
         letra_entry.delete(0, tk.END)
-        print("Digite uma letra valida")
+        mensagem["text"] = "Por favor, digite uma letra valida"
         return
     
     letra = letra_entry.get()
-    print(f"Letra: {letra}")
+    mensagem["text"] = f"Letra: {letra}"
         
     letra_entry.delete(0, tk.END)
 
@@ -55,32 +69,39 @@ def jogar():
             pontos += 1
     else:
         erros += 1
-        print("Letra errada!")
+        mensagem["text"] = "Letra errada!"
+        esquarteja(erros)
 
-    palavra_oculta_label["text"] = atualiza_palavra()
+    palavra_atualizada_label["text"] = atualiza_palavra()
     dica_label["text"] = f"Dica: {dica}"
+    pontos_label["text"] = f"Pontos: {pontos}"
     erros_label["text"] = f"Erros: {erros}"
 
     if atualiza_palavra() == palavra:
-        print(f"Parabéns! Você acertou a palavra '{palavra}' e fez {pontos} pontos.")
+        mensagem["text"] = f"Parabéns! Você acertou a palavra '{palavra}' e fez {pontos} pontos."
+        letra_entry.destroy()
+        botao.destroy()
         return
-    if erros == 6:
-        print(f"Que pena, você perdeu! A palavra era '{palavra}'.")
+    if erros >= 6:
+        mensagem["text"] = f"Que pena, você perdeu! A palavra era '{palavra}'."
+        letra_entry.destroy()
+        botao.destroy()
         return
 
-palavra, dica, palavraOculta = sortear_palavra()
+palavra, dica, palavra_oculta = sortear_palavra()
 erros = 0
 pontos = 0
 letras_acertadas = ""
 
 janela = tk.Tk()
-janela.title("Jogo da Forca")
+janela.geometry("800x600")
+janela.title("Jogo do Esquartejado")
 
 dica_label = tk.Label(janela, text = f"Dica: {dica}", font=("Helvetica", 14))
 dica_label.pack()
 
-palavra_oculta_label = tk.Label(janela, text = palavraOculta, font=("Helvetica", 24))
-palavra_oculta_label.pack()
+palavra_atualizada_label = tk.Label(janela, text = palavra_oculta, font=("Helvetica", 24))
+palavra_atualizada_label.pack()
 
 letra_entry = tk.Entry(janela, font=("Helvetica", 24))
 letra_entry.pack()
@@ -88,9 +109,25 @@ letra_entry.pack()
 botao = tk.Button(janela, text="Enviar", command=jogar)
 botao.pack()
 
+mensagem = tk.Label(janela, text="", font=("Helvetica", 14))
+mensagem.pack()
+
+pontos_label = tk.Label(janela, text = f"Pontos: {pontos}", font=("Helvetica", 14))
+pontos_label.pack()
+
 erros_label = tk.Label(janela, text = f"Erros: {erros}", font=("Helvetica", 14))
 erros_label.pack()
 
-# atualizar_interface(palavra, letras_acertadas, dica, erros)
+canvas = tk.Canvas(janela, width=300, height=400)
+canvas.pack()
+canvas.create_line(50, 350, 250, 350, width=5)
+canvas.create_line(100, 350, 100, 100, width=5)
+canvas.create_line(200, 350, 200, 100, width=5)
+cabeca = canvas.create_oval(125, 100, 175, 150, fill="white", width=5)
+tronco = canvas.create_line(150, 150, 150, 250, width=5)
+braco_esq = canvas.create_line(150, 200, 100, 175, width=5)
+braco_dir = canvas.create_line(150, 200, 200, 175, width=5)
+perna_esq = canvas.create_line(150, 250, 100, 300, width=5)
+perna_dir = canvas.create_line(150, 250, 200, 300, width=5)
 
 janela.mainloop()
